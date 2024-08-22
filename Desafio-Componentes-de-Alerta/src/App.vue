b13bb552cf3deaf165b679614cf8a49ce19dc0bd
Manoel Ívisson <manoelivisson4@hotmail.com>
2024-08-22 11:13:45 -0300

    feature: Criacao do desafio e do componente AppAlerta

---
+++ Desafio-Componentes-de-Alerta/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import AppAlerta from './components/AppAlerta.vue'
+
+</script>
+
+<template>
+  <app-alerta tipo="info"> Novas opurtunidades de emprego chegaram! </app-alerta>
+</template>
+
---
+++ Desafio-Componentes-de-Alerta/src/components/AppAlerta.vue
@@ -0,0 +1,13 @@
+<script setup>
+  const props = defineProps({
+    tipo: {type: String, required: true}
+  })
+
+</script>
+
+<template>
+  <div class="alerta">
+    <p><slot></slot></p>
+    <button>x</button>
+  </div>
+</template>
