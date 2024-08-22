a96c790ece01b181bf1ec6276d512c89383b0757
Manoel Ívisson <manoelivisson4@hotmail.com>
2024-08-22 15:20:53 -0300

    feature: Conlusao do desafio.

--- Desafio-Componentes-de-Alerta/src/App.vue
+++ Desafio-Componentes-de-Alerta/src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
-import AppAlerta from './components/AppAlerta.vue'
+  import AppAlerta from './components/AppAlerta.vue'
 
+  function aviso() {
+    console.log('Alerta Fechado')
+  }
 </script>
 
 <template>
-  <app-alerta tipo="info"> Novas opurtunidades de emprego chegaram! </app-alerta>
+  <app-alerta tipo="info" @fechar-alerta="aviso"> Novas opurtunidades de emprego chegaram! </app-alerta>
+  <app-alerta tipo="sucesso" @fechar-alerta="aviso"> Parabéns, você foi contratado! </app-alerta>
+  <app-alerta tipo="aviso" @fechar-alerta="aviso"> Teste liberado </app-alerta>
+  <app-alerta tipo="erro" @fechar-alerta="aviso"> Houve uma falha, tente novamente mais tarde </app-alerta>
 </template>
 
--- Desafio-Componentes-de-Alerta/src/components/AppAlerta.vue
+++ Desafio-Componentes-de-Alerta/src/components/AppAlerta.vue
@@ -1,13 +1,53 @@
 <script setup>
+  import { ref, onMounted, computed } from 'vue'
+
   const props = defineProps({
     tipo: {type: String, required: true}
   })
 
+  const alertaVisivel = ref(true)
+
+  const propriedadesAlerta = computed(() => {
+    if (props.tipo == 'info') {
+      return {
+        icone: 'info', 
+        estilo: 'info'}
+    } else if (props.tipo == 'sucesso') {
+      return {
+        icone: 'check_circle', 
+        estilo: 'sucesso'}
+    } else if (props.tipo == 'aviso') {
+      return {
+        icone: 'warning', 
+        estilo: 'aviso'}
+    } else if (props.tipo == 'erro') {
+      return {
+        icone: 'error', 
+        estilo: 'erro'}
+    }
+  })
+
+  const emit = defineEmits(['fecharAlerta'])
+  
+  function fecharAlerta() {
+    alertaVisivel.value = false
+    emit('fecharAlerta')
+  }
+
 </script>
 
 <template>
-  <div class="alerta">
-    <p><slot></slot></p>
-    <button>x</button>
+  <div v-show="alertaVisivel" class="alerta" :class="propriedadesAlerta.estilo">
+    <p class="comunicado">
+      <span class="material-symbols-outlined">
+        {{ propriedadesAlerta.icone }}
+      </span> 
+      <slot></slot>
+    </p>
+    <button @click="fecharAlerta">
+      <span class="material-symbols-outlined">
+        close
+      </span>
+    </button>
   </div>
 </template>
